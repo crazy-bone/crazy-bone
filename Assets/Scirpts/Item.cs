f041ee243566b21eb49ab2912fea9b58cc7460dd
Gojeongtae <giwigi@naver.com>
2021-04-07 21:57:09 +0900

    아이템 생성 및 프리팹 만들기
    1. 아이템 생성 및 프리팹 만들기

--- Assets/Scirpts/Item.cs
+++ Assets/Scirpts/Item.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-   public enum Type { Ammo, Coin, Grenade, Heart, Weapon};
+    public enum Type { Ammo, Coin, Grenade, Heart, Weapon};
+    public Type type;
+    public int value;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+        
+    }
+
 }
