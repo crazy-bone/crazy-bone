ef95ad93481c064871ac906b832ac4369b96e3ba
Gojeongtae <giwigi@naver.com>
2021-04-14 23:01:26 +0900

    grenade 를 먹으면 활성화 시키기
    1. grenade를 각각 저번에 만든 grenade 게임 오브젝트 집합에 (player 요소들) 넣어주고 case에 맞게 grenade type과 충돌했을때, hasGrenade(int형)을 하나씩 true값으로 바꿔준다. 물론 초기값은 비활성화 시킨다. 한개 부딪힐때마다 SetActive함수가 실행되는 것을 확인할 수 있고 총 4개까지 가능하다.

--- Assets/Scirpts/Player.cs
+++ Assets/Scirpts/Player.cs
@@ -238,6 +238,7 @@ public class Player : MonoBehaviour
                         health = maxHealth;
                     break;
                 case Item.Type.Grenade:
+                    grenades[hasGrenades].SetActive(true);
                     hasGrenades += item.value;
                     if (hasGrenades > maxHasGrenades)
                         hasGrenades = maxHasGrenades;
