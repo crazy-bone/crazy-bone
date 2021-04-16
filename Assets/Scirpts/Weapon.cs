0d34b83d3a8b70b55b390107df3fb2f78e989417
Gojeongtae <giwigi@naver.com>
2021-04-16 22:50:24 +0900

    코루틴 함수의 사용 및 IEnumerator은 열거형 함수안의 yield return을 통해 invoke를 대신하기
     (주석단 부분은 깃허브로 인식이 안되는 것 같다.)
     우리는 지금까지 어떠한 행동을 실행 시킬때 1.Use함수 실행하고 함수 안에서 Swing을 불러옴 2.Swing함수실행후 닫음 3.다시 Use함수의 순서대로 실행해왔다. 그렇지만 그렇게 하면 중간에 텀을 주고 싶을때 Invoke함수밖에 쓰지 못한다. 중간중간에 계속 쉬고 싶을때는 어떻게 해야 할까?
     또한 1. 2. 3.의 번거로운 순서 대신에 코루틴함수, 즉 1번과 2번이 함께 실행되는 함수를 통해 동시에 실행하고자 한다. IEnumerator은 열거형 함수이자 코루틴 함수이다. Void가 아닌 함수를 사용한 것이다.
    Yield return 값을 null을 줌으로 1프레임을 중간에 쉴 수 있다. 우리는 이제 Invoke함수를 사용하지 않아도 된다!
    또한 1프레임만 쉬기 싫을때는 return값을 new WaitForSecond(t)를 사용할 수 있다 이때 t는 float값을 주어야 한다.

--- Assets/Scirpts/Weapon.cs
+++ Assets/Scirpts/Weapon.cs
@@ -12,4 +12,28 @@ public class Weapon : MonoBehaviour
     public BoxCollider meleeArea;
     public TrailRenderer trailEffect;
 
+    public void Use()
+    {
+        if(type == Type.Melee)
+        {
+            Swing();
+        }
+    }
+
+    IEnumerator Swing()
+    {
+        //1
+        yield return null; //1프레임 대기
+        //2
+        yield return null; //1프레임 대기
+        yield return new WaitForSeconds(0.1f); //0.1초 대기
+        yield break; //break로 코루틴 탈출 가능 그러나 break 밑의 함수는 
+        //실행 안되기 때문에 주의할 것.
+
+    }
+
+    //use() 메인루틴 -> swing() 서브루틴 -> Use() 메인루틴
+    //use() 메인루틴 + swing() 코루틴(같이 실행되는 것임. co)
+    //우리는 코루틴을 쓸거임, 열거형 함수 IEnumerator, 결과를 전달하는 yield
+    //
 }
