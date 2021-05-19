569fb4a0cdf3d8f561c4fc8a3e987987b2c2c6da
Gojeongtae <giwigi@naver.com>
2021-05-19 21:20:46 +0900

    피격 효과, 충돌 이벤트 (incomplete)
    피격 로직과 충돌이벤트 구현 중에 아직 미완성된 코드입니다.

---
+++ Assets/Scirpts/Enemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+
+    Rigidbody rigid;
+    BoxCollider boxCollider;
+    Material mat;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Melee")
+        {
+            Weapon weapon = other.GetComponent<Weapon>();
+            curHealth -= weapon.damage;
+            StartCoroutine(OnDamage());
+        }
+         else if (other.tag == "Bullet")
+        {
+            Bullet bullet = other.GetComponent<Bullet>();
+            curHealth -= bullet.damage;
+            StartCoroutine(OnDamage());
+        }
+    }*/
+
+    IEnumerator OnDamge()
+    {
+        mat.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+
+        if(curHealth > 0)
+        {
+
+        }
+    }
+
+   
+}
