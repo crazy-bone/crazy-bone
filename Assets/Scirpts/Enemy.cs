403df88747c50137a6c58678b5e5cb233d1f88f7
Gojeongtae <giwigi@naver.com>
2021-05-29 16:55:13 +0900

    Revert "Revert "AI를 이용한 Nav Mesh Agent로 플레이어 추적 구현""
    This reverts commit 0795200dd9235a21631560e727788234e947761e.

--- Assets/Scirpts/Enemy.cs
+++ Assets/Scirpts/Enemy.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
+    public Transform target;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
     Material mat;
+    NavMeshAgent nav;
+
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
+        nav = GetComponent<NavMeshAgent>();
+    }
 
+    void Update()
+    {
+        nav.SetDestination(target.position);
     }
 
     /*private void OnTriggerEnter(Collider other)
