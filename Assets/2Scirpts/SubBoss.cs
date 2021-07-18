1e94d67dbe7e9a2f82d1cb21cf2f2a1932e1b84c
Seongbum Seo <sBum.Seo@gmail.com>
2021-07-18 19:18:44 +0900

    서브웬디고 공격 추가

--- Assets/2Scirpts/SubBoss.cs
+++ Assets/2Scirpts/SubBoss.cs
@@ -5,9 +5,19 @@ using UnityEngine.AI;
 
 public class SubBoss : MonoBehaviour
 {
-    public Transform target;
+    public Transform target; // 공격 대상
+    public float attackDistance; // 공격할 거리
+    public BossMissile missileTemplate;
+    public Transform[] missilePorts;
 
-    NavMeshAgent navMeshAgent;
+    private enum Status
+    {
+        IDLE,
+        Attack
+    }
+
+    private NavMeshAgent navMeshAgent;
+    private Status status = Status.IDLE;
 
     void Start()
     {
@@ -16,6 +26,36 @@ public class SubBoss : MonoBehaviour
 
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        switch (status)
+        {
+            case Status.IDLE:
+                float distance = Vector3.Distance(transform.position, target.position);
+                if (distance < attackDistance)
+                    StartCoroutine(Attack());
+                else
+                    navMeshAgent.SetDestination(target.position);
+                break;
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        status = Status.Attack;
+        navMeshAgent.enabled = false;
+        Debug.Log("서브웬디고 정지");
+
+        yield return new WaitForSeconds(2f);
+
+        Debug.Log("서브보스 미사일 발사");
+        foreach (Transform missilePort in missilePorts)
+        {
+            BossMissile missile = Instantiate<BossMissile>(missileTemplate, missilePort.position, missilePort.rotation);
+            missile.target = target;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        status = Status.IDLE;
+        navMeshAgent.enabled = true;
     }
 }
