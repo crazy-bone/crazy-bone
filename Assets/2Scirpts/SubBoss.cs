8b3b4062fa55269ca5cba4761ab1e0a352f61a0b
Seongbum Seo <sBum.Seo@gmail.com>
2021-07-18 18:03:10 +0900

    서브웬디고 NavMeshAgent 적용

--- Assets/2Scirpts/SubBoss.cs
+++ Assets/2Scirpts/SubBoss.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SubBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+
+    NavMeshAgent navMeshAgent;
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        navMeshAgent.SetDestination(target.position);
     }
 }
