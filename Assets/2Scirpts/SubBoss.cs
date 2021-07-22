5a69b329a290e95ad9c6097b312e66c245d33140
Seongbum Seo <sBum.Seo@gmail.com>
2021-07-22 20:41:12 +0900

    접촉 데미지 추가

--- Assets/2Scirpts/SubBoss.cs
+++ Assets/2Scirpts/SubBoss.cs
@@ -5,9 +5,16 @@ using UnityEngine.AI;
 
 public class SubBoss : MonoBehaviour
 {
-    public Transform target; // 공격 대상
+    /// <summary> 플레이어 </summary>
+    public Player player;
+    /// <summary> 공격 대상 </summary>
+    public Transform target;
+    /// <summary> 미사일 템플릿 Prefab, 클론하여 사용됨 </summary>
     public BossMissile missileTemplate;
+    /// <summary> 미사일 발사 위치 </summary>
     public Transform[] missilePorts;
+    /// <summary> 접촉 데미지 </summary>
+    public int contactDamage = 10;
 
     private enum Status
     {
@@ -34,6 +41,15 @@ public class SubBoss : MonoBehaviour
                 if (distance <= navMeshAgent.stoppingDistance)
                     StartCoroutine(Attack());
                 break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            player.health -= contactDamage;
+            Destroy(gameObject);
         }
     }
 
