9f5455010c06921d1137889fa90b9dcaefbdda07
Seongbum Seo <sBum.Seo@gmail.com>
2021-07-22 09:32:38 +0900

    탄알 회전속도 조정

--- Assets/2Scirpts/BossMissile.cs
+++ Assets/2Scirpts/BossMissile.cs
@@ -19,11 +19,12 @@ public class BossMissile : Bullet
         if (target != null)
         {
             Vector3 targetPosition = target.position + new Vector3(0f, 5f, 0f);
-            Vector3 direction = target.position - transform.position;
+            Vector3 direction = targetPosition - transform.position;
 
             //transform.rotation.SetLookRotation(target.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            //transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
         }
     }
 }
