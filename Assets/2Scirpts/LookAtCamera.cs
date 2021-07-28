4a148772b5ee320979d8110439abdf5122c88009
Seongbum Seo <sBum.Seo@gmail.com>
2021-07-28 14:44:27 +0900

    GUI Rotation 고정

---
+++ Assets/2Scirpts/LookAtCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    public Camera camera;
+
+    void Update()
+    {
+        Vector3 v = camera.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(camera.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
