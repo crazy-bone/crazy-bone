5ec4a4c16c053e55e151cfc56745b78175ca6515
Seongbum Seo <sbum.seo@gmail.com>
2021-11-14 13:11:13 +0900

    CSV리더 추가

---
+++ Assets/2Scirpts/Cutscene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
